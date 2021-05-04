5141c522945a2ce7003d9dbe7c09a459aff78a4b
xiongjiajie <3293225650@qq.com>
2021-05-04 23:52:23 +0800

    '初始化'

---
+++ README.md
@@ -0,0 +1,6 @@
+##熊佳杰
+
+##资料文档
+https://github.com/xiongjiajie/community2
+git@github.com:xiongjiajie/community2.git
+## 工具
---
+++ src/main/java/com/xjj/community2/Community2Application.java
@@ -0,0 +1,12 @@
+package com.xjj.community2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Community2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Community2Application.class, args);
+    }
+
+}
